fb4780a796bc37b14903da841eecfc32449b351d
Jake Treacher <git@jaketreacher.com>
2021-10-20 09:33:16 -0700

    wip: init

---
+++ .eleventy.js
@@ -0,0 +1,10 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.setBrowserSyncConfig({
+    files: "./_site/css/**/*.css",
+  });
+  return {
+    dir: {
+      input: "src",
+    },
+  };
+};
